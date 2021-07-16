970ea4c9e13988f9b669f77c0137050063bf326a
Shahid <mshahidgo@gmail.com>
2021-07-16 23:27:39 +0530

    dotenv

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
-//const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const path = require('path');
 
 const errorMiddleware = require('./middlewares/errors');
 
 //Setting up config file
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
-//dotenv.config({ path: 'backend/config/config.env'})
+//if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+dotenv.config({ path: 'backend/config/config.env'})
 
 app.use(express.json({ limit: "50mb" })); 
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
